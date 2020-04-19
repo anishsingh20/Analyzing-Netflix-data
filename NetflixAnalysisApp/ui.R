8dcb67816d9141897a3d7cbc5f9c078cc2a77f8a
anishsingh20 <loveeanish@gmail.com>
2020-04-19 10:52:31 +0530

    made the project directory

---
+++ NetflixAnalysisApp/ui.R
@@ -0,0 +1,11 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
